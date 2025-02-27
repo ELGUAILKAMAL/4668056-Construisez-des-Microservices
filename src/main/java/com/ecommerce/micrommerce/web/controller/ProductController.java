554d9b3ca2b283feb7c811caf3b04131a166a9e4
ELGUAILKAMAL <elguailkamal@gmail.com>
2025-02-27 21:46:59 +0100

    Ajout de la methode de tri des produits par nom

--- src/main/java/com/ecommerce/micrommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/micrommerce/web/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.micrommerce.web.controller;
 
 import com.ecommerce.micrommerce.web.dao.ProductDao;
+import com.ecommerce.micrommerce.web.dto.marges;
 import com.ecommerce.micrommerce.web.exceptions.ProduitIntrouvableException;
 import com.ecommerce.micrommerce.web.model.Product;
 import io.swagger.annotations.Api;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @Api( "API pour les opérations CRUD sur les produits.")
@@ -72,5 +74,9 @@ public class ProductController {
             marge.add(new marges(p,p_marge));
         }
         return marge;
+    }
+    @GetMapping(value="/ProductsTries")
+    public List<Product> trierProduitsParOrdreAlphabetique(){
+        return productDao.findAllByOrderByNomAsc();
     }
 }
